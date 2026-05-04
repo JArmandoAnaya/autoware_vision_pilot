0157b012a6985d35c6cb685ff414362ab3bd3670
TranHuuNhatHuy <huy9515@gmail.com>
2026-05-04 12:14:34 +0900

    define namespace and briefings

--- VisionPilot/development_releases/1.0/modules/middleware_interfaces/ros2_interface/camera_subscriber/include/camera_subscriber/ros2_to_opencv.hpp
+++ VisionPilot/development_releases/1.0/modules/middleware_interfaces/ros2_interface/camera_subscriber/include/camera_subscriber/ros2_to_opencv.hpp
@@ -1,4 +1,20 @@
 #ifndef VISIONPILOT_ROS2_TO_OPENCV_HPP
 #define VISIONPILOT_ROS2_TO_OPENCV_HPP
 
+namespace camera_subscriber {
+
+    /**
+    * @class ROS2ImageSubscriber
+    * @brief ROS2 node that subscribes to `sensor_msgs/image` topics and 
+    *        converts ROS2 image message to OpenCV image format (cv::Mat).
+    * 
+    * Features:
+    * - Subscribes to ROS2 image topics (from any source - simulator, camera, hardware, etc.)
+    * - Converts ROS2 image messages to OpenCV cv::Mat format
+    * - Thread-safe conversion and data handling
+    * - Supports various image encodings (RGB, BGR, grayscale, etc.)
+    */
+
+}
+
 #endif //VISIONPILOT_ROS2_TO_OPENCV_HPP
