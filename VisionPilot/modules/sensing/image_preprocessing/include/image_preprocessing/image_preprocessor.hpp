d7e48b3c0bb245d5f0c75a57ef634cd6f1434f09
Pranav Doma <pranavreddy2327@gmail.com>
2026-06-26 12:59:01 -0400

    on the fly H calc

--- VisionPilot/modules/sensing/image_preprocessing/include/image_preprocessing/image_preprocessor.hpp
+++ VisionPilot/modules/sensing/image_preprocessing/include/image_preprocessing/image_preprocessor.hpp
@@ -9,11 +9,17 @@ public:
 
     ~ImagePreprocessor() = default;
 
-    void preprocess(const cv::Mat &image, cv::Mat &warped_image, cv::Mat &resized_image, const cv::Size &size) const;
+    void preprocess(const cv::Mat &image, cv::Mat &warped_image, cv::Mat &resized_image, const cv::Size &size);
+
+    // C maps raw camera pixel → warped 1024×512 BEV (perspective transform).
+    const cv::Mat& C_mat()   const { return C; }
+    // Raw frame dimensions seen on the last preprocess() call.
+    cv::Size       raw_size() const { return raw_size_; }
 
 private:
     std::string homography_C_matrix_path;
-    cv::Mat C;
+    cv::Mat     C;
+    cv::Size    raw_size_;
 };
 
 #endif //VISIONPILOT_IMAGE_PREPROCESSOR_HPP
